4a2d07cdb2761119854d9437406c9be33e70df57
Betty Lopez <elisabetlopezpons@gmail.com>
2024-06-18 15:56:39 +0200

    Install shadcn library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import useApi from "./services/useApi";
 import { URL_ALL } from "./config/urls";
+import { Button } from "./components/ui/button";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
     Hello world!
       </h1>
+      <Button>Click me</Button>
     </>
   )
 }
