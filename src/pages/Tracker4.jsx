5a4d3188653b05382227c0405b01bde740bfef08
jess-ar <jessica18_24@hotmail.com>
2024-06-26 23:45:00 +0200

    adding styles to buttons and cards

--- src/pages/Tracker4.jsx
+++ src/pages/Tracker4.jsx
@@ -1,56 +1,53 @@
-import  { useState, useEffect } from 'react';
-import WidgetHead from '../components/widgets/WidgetHead';
-import WorldItem from '@/components/worldWideData/WorldItem';
+import { useState, useEffect } from 'react';
+import WidgetHead from '@/components/tracker/widgetHead/WidgetHead';
+import GlobalItem from '@/components/tracker/globalData/globalItem/GlobalItem';
 import useApi from '@/services/useApi';
 import { URL_COUNTRIES } from '@/config/urls';
 import ButtonBadge from '@/components/buttonBadge/ButtonBadge';
 
 const Tracker4 = () => {
-  const data = useApi (URL_COUNTRIES) 
+  const data = useApi(URL_COUNTRIES)
   const displayData = data.sort((a, b) => b.deaths - a.deaths).slice(0, 9)
-  
-  const [selectedCountry, setSelectedCountry] = useState("");
-  const filterCountry = (country) => {
-    console.log(country)
-    setSelectedCountry (country.country)
-    setCountryData (displayData.find(country => country.country === selectedCountry))
-  }  
 
+  const [selectedCountry, setSelectedCountry] = useState("");
   const [countryData, setCountryData] = useState({});
 
-  useEffect(() => {
-    setCountryData(displayData[0])
-    
-  },[displayData]
-  )
-  /* 
-  const hasSelectedCountry = useRef(false); // Para evitar bucles infinitos
-  const countryData = displayedData.find(country => country.country === selectedCountry);
+  const filterCountry = (country) => {
+    console.log(country);
+    setSelectedCountry(country.country);
+    setCountryData(displayData.find(item => item.country === country.country));
+  };
 
-  if (!countryData) {
+  useEffect(() => {
+    if (displayData.length > 0) {
+      setCountryData(displayData[0]);
+    }
+  }, [displayData]);
+
+  if (!data) {
     return <div>Loading...</div>;
   }
- */
+
   return (
     <>
-      <WidgetHead text="Covid-19 Country Wise - Tabs" />
-      <ul>
-      {
-        displayData.map ((item, index) => {
-          return (<ButtonBadge handleClick={filterCountry} key={index} country={item} />
-          )
-        }
-        ) 
-      }
-      </ul>
-
-      <div className="flex flex-wrap justify-around mb-4">
-        <WorldItem title="Total Cases" value={countryData?.cases} imageSrc="\public\assets\images\icons\covid-defult.svg"/>
-        <WorldItem title="Total Deaths" value={countryData?.deaths} imageSrc="\public\assets\images\icons\covid-defult.svg" />
-        <WorldItem title="Total Recovered" value={countryData?.recovered} imageSrc="\public\assets\images\icons\covid-defult.svg" />
-        <WorldItem title="Total Active" value={countryData?.active} imageSrc="\public\assets\images\icons\covid-defult.svg" />
+      <div className="w-full">
+        <WidgetHead text="Covid-19 Country Wise - Tabs" />
+        <ul className="flex flex-wrap mb-4 sm:justify-start">
+    {displayData.map((item, index) => (
+        <ButtonBadge handleClick={filterCountry} key={index} country={item} />
+    ))}
+</ul>
       </div>
-      </>
+
+      <div className="grid justify-start grid-cols-2 lg:grid-cols-4 ">
+        <GlobalItem title="Total Cases" value={countryData?.cases} imgSrc="/assets/images/icons/covid-defult.svg" valueClassName="text-[color:var(--col-common-blue)]"/>
+        <GlobalItem title="Total Deaths" value={countryData?.deaths} imgSrc="/assets/images/icons/covid-red.svg" valueClassName="text-[color:var(--col-bright-red)]"/>
+        <GlobalItem title="Total Recovered" value={countryData?.recovered} imgSrc="/assets/images/icons/covid-green.svg" valueClassName="text-[color:var(--col-green)]"/>
+        <GlobalItem title="Total Active" value={countryData?.active} imgSrc="/assets/images/icons/covid-blue.svg" valueClassName="text-[color:var(--col-bright-blue)]"/>
+        <GlobalItem title="New Cases" value={countryData?.todayCases} imgSrc="/assets/images/icons/covid-orange.svg" valueClassName="text-[color:var(--col-orange)]"/>
+        <GlobalItem title="New Deaths" value={countryData?.todayDeaths} imgSrc="/assets/images/icons/covid-redark.svg" valueClassName="text-[color:var(--col-wine)]" />
+      </div>
+    </>
   );
 };
 
