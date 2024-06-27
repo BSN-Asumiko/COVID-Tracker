482d48e98d90c1f8b584853b7ef79c6af716fe31
BCN-Asumiko <yakoshko@gmail.com>
2024-06-27 16:22:45 +0200

    finished CT-45: add styles to carousel

--- src/components/homePageComponents/carousel/CarouselComponent.jsx
+++ src/components/homePageComponents/carousel/CarouselComponent.jsx
@@ -4,15 +4,15 @@ import CarouselCard from "./CarouselCard";
 
 const CarouselComponent = () => {
     return (
-    <section className="w-full overflow-hidden relative">
+    <section className="w-full overflow-hidden z-[-1]">
         <Carousel className="w-full m-auto"
                 opts={{
                 align: "start",
                 loop: true,
                 }}>
-                <CarouselContent className="w-full flex gap-5 h-[20em]">
+                <CarouselContent className="w-full flex gap-2 h-[20em]">
                 {carouselData.map((object, index) => (
-                            <CarouselItem key={index} className="flex-shrink-0 lg:basis-1/4 md:basis-1/3 sm:basis-1/2 basis-full">
+                            <CarouselItem key={index} className=" lg:basis-1/4 md:basis-1/3 sm:basis-1/2 ">
                                 <CarouselCard icon={object.icon} title={object.title} text={object.text} color={object.color}/>
                             </CarouselItem>    
                         ))}
@@ -22,43 +22,5 @@ const CarouselComponent = () => {
     )
 }
 
-/* <Carousel className="w-full"
-                opts={{
-                align: "start",
-                loop: true,
-                }}>
-                <CarouselContent className="w-full flex gap-7">
-                {carouselData.map((object, index) => (
-                            <CarouselItem key={index} className="flex-shrink-0 lg:basis-1/4 md:basis-1/3 sm:basis-1/2 basis-full">
-                                <CarouselCard icon={object.icon} title={object.title} text={object.text} color={object.color}/>
-                            </CarouselItem>    
-                        ))}
-                </CarouselContent>
-            </Carousel> */
 
-/* const CarouselComponent = () => {
-    return (
-    <>
-        <Carousel className="w-[150%] m-auto" 
-            opts={{
-            align: "start",
-            loop: true,
-            }}>
-            <CarouselContent className="w-full flex gap-8">
-                {carouselData.map((object, index) => (
-                <CarouselItem key={index} className="flex-shrink-0 lg:basis-1/6 md:basis-1/4 sm:basis-1/2 basis-full">
-                    <CarouselCard 
-                        icon={object.icon} 
-                        title={object.title} 
-                        text={object.text} 
-                        color={object.color}
-                    />
-                </CarouselItem>    
-                ))}
-            </CarouselContent>
-                </Carousel>
-    </>
-    )
-}
- */
 export default CarouselComponent
--- src/components/homePageComponents/carousel/CarouselSection.jsx
+++ src/components/homePageComponents/carousel/CarouselSection.jsx
@@ -8,9 +8,24 @@ export function CarouselSection() {
                 <p className=" text-[small] text-[color:var(--col-body)] font-semibold tracking-[0.1em] mb-[1em]">App Feature</p>
                 <h1 className=" text-[xx-large] font-bold text-[color:var(--col-body)] leading-[1.2em]">Basic Feature You Will Get When You Use</h1>
             </div>
-            <CarouselComponent/>
+            <section className="relative w-full overflow-hidden h-[20em]">
+                <div className="absolute inset-0 flex justify-between z-20 pointer-events-none">
+                    <CarouselMask />
+                    <CarouselMask />
+                </div>
+                <CarouselComponent/>
+            </section>
+            
         </section>
     )
 }
+
+
+const CarouselMask = (className) => {
+    return (
+    <div className="bg-[color:var(--col-home-main)] h-full w-[5%] opacity-[60%] blur-[0.05em] z-[1]" ></div>
+    )
+}
+
 
 export default CarouselSection
