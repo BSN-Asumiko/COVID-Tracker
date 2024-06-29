bca97cccf3bd2bcabd55cb1145fda7a42fefa8c9
Betty Lopez <elisabetlopezpons@gmail.com>
2024-06-29 10:44:28 +0200

    Navbar hamburger + responsive

--- src/components/hompage/navbar/Navbar.jsx
+++ src/components/hompage/navbar/Navbar.jsx
@@ -13,25 +13,27 @@ const Navbar = () => {
 
 
     return (
-        <nav className={`*:{isOpen ? 'isOpen' : ''} w-screen flex justify-between py-[1em] items-center @container`}>
+        <nav className=" w-screen flex justify-between py-[1em] items-center">
             <div className="w-[30%] ml-[7cqw] ">
                 <img src="/assets/images/logo.png" alt="Covimap logo"/>
             </div>
-            <div className="flex justify-end content-end ml:hidden">
+           
+            <div className="w-[70%] flex ml:justify-around justify-end text-[1.1em]">
+            <div className="w-[70%] flex justify-end ml:hidden mr-9">
                 <button onClick={toggleMenu}>
-                    <span className="icon" >&#xf0d7; </span>+
+                    <span className="icon" >&#xE6C3;</span>
                 </button>
             </div>
-            <div className="w-[20%] ml:w-[70%] flex flex-row justify-around text-[1.1em]">
-                <ul className={`${isOpen ? 'block' : 'hidden'}  text-left bg-[color:var(--col-body)] ml:flex flex-row items-center gap-[1.5em]`}>
+                <ul className={`${isOpen ? 'block' : 'hidden'} absolute px-[6cqw] py-[2em] ml:static text-left bg-[color:var(--col-body)] ml:flex ml:flex-row mt-4 ml:mt-0 items-center gap-[1.5em]`}>
                     <NavItem content = "Home" icon="&#xf0d7;"/>
                     <NavItem content = "Prevention"/>
                     <NavItem content = "Qurantine"/>
                     <NavItem content = "Pages" icon="&#xf0d7;"/>
                     <NavItem content = "About"/>
                     <NavItem content = "Help"/>
-                    <NavLink to="/tracker" className="px-[4cqw] py-[0.7em] rounded-[1.5em] text-[color:var(--col-body)] bg-[color:var(--col-common-blue)]">
-                Tracker</NavLink>
+                    <div className="ml:mt-0 ml:mb-0 mt-6 mb-2">
+                        <NavLink to="/tracker" className="px-[4cqw] py-[0.7em] rounded-[1.5em] text-[color:var(--col-body)] bg-[color:var(--col-common-blue)] ">
+                Tracker</NavLink></div>
                 </ul>
                 
             </div>
